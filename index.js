5b79240efcf42041e771932654c26962e48423bd
Beth <bethmelmtv@gmail.com>
2022-07-12 10:34:51 -0700

    mob programmed reverseWords function

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-function reverseWords(sentence) {}
-
 module.exports = { reverseWords };
+
+function reverseWords(sentence) {
+  return (
+    sentence
+      .split(" ") //converting a sentence to an array of words
+      // .map((letter) => sentence.reverse()) // mapping through the array of
+      .map((letter) => letter.split("").reverse().join(""))
+      .join(" ")
+  );
+  //emptry string
+  // map // for loop through a string(sentence), for each letter,
+  //append it to the emptry string
+  //return empty string
+  return;
+}
